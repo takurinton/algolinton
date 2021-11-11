b81b55699e21058dacdd013c11990eb238a017c6
takurinton <takurinton@takurinton.com>
2021-11-11 11:15:33 +0900

    add: react-instantsearch-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-
 import algoliasearch from 'algoliasearch';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 const APPLICATION_ID = import.meta.env.VITE_APPLICATION_ID as string;
 const ADMIN_APT_KEY = import.meta.env.VITE_ADMIN_API_KEY as string;
 
 function App() {
   const client = algoliasearch(APPLICATION_ID, ADMIN_APT_KEY);
-  const index = client.initIndex('takurinton');
-
-  const [state, setState] = useState({ search: '' });
-  const [res, setRes] = useState<any[] | undefined>([]);
-
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const v = event.target.value;
-    setState({ search: v });
-
-    index
-      .search(v)
-      .then(({ hits }) => {
-        setRes(hits);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
   return (
-    <div style={{ margin: '100px 0', textAlign: 'center' }}>
-      <input type="text" name="search" onChange={handleChangeValue} style={{ fontSize: '1.4rem' }} placeholder='キーワードを入力'/>
-      <div>
-        {
-          res?.map((r, index) => (
-            <div key={index.toString()}>
-              <h1>{r.title}</h1>
-              <p>{r.pub_date}</p>
-              <hr />
-            </div>
-          ))
-        }
-      </div>
-    </div>
+    <InstantSearch searchClient={client} indexName="takurinton">
+      <SearchBox />
+      <Hits />
+    </InstantSearch>
   )
 }
 
